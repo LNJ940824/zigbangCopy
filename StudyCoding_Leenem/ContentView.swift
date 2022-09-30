2b3601b4de8c2c2a0cdb4d894ee2391f281acbe2
Namjoo Lee <dltpdl12@naver.com>
2022-09-30 15:19:05 +0900

    Initial Commit

---
+++ StudyCoding_Leenem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyCoding_Leenem
+//
+//  Created by 이남주 on 2022/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyCoding_Leenem/StudyCoding_LeenemApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCoding_LeenemApp.swift
+//  StudyCoding_Leenem
+//
+//  Created by 이남주 on 2022/09/30.
+//
+
+import SwiftUI
+
+@main
+struct StudyCoding_LeenemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
